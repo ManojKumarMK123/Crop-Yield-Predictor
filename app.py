1aeaeda28ab4d7166c01a48d515375020f17457e
Manoj Kumar Chouhan <114495263+ManojKumarMK123@users.noreply.github.com>
2025-01-27 11:53:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
-
+from sklearn._loss import _loss
 st.set_page_config(page_title="Crop Yield Predictor",layout="wide",page_icon = 'climate1.png')
 st.markdown(
         r"""
